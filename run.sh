b3b44bb05852eea14561c3525a8be9f5987d668f
Jeff Kent <jeff@jkent.net>
2019-07-08 15:42:00 -0500

    run.sh: setup venv if needed

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 export PIPENV_VENV_IN_PROJECT=1
 export FLASK_ENV=development
+if [ ! -f .venv ]; then
+  pipenv install
+fi
 if [ ! -f instance/jkent_net.db ]; then
   pipenv run flask init-db
   pipenv run flask add-user --password password --is-admin jeff@jkent.net
