e006e0dfb847621fd4c69dd99798b7f054e8974b
Jeff Kent <jeff@jkent.net>
2019-07-20 10:29:00 -0500

    admin stuff

--- jkent_net/views/admin.py
+++ jkent_net/views/admin.py
@@ -1,13 +1,22 @@
-from flask import Blueprint, Markup, Response, abort, current_app, render_template, request, send_file
+from flask import Blueprint, render_template
+from flask_menu import current_menu, register_menu
+
 
 __all__ = ['bp']
 
 bp = Blueprint('admin', __name__)
 
-@bp.route('')
-def index():
-    return render_template('admin/index.html')
+@bp.route('settings')
+@register_menu(bp, '.admin.settings', 'Settings')
+def settings():
+    return render_template('admin/settings.html')
 
 @bp.route('pages')
+@register_menu(bp, '.admin.pages', 'Pages')
 def pages():
     return render_template('admin/pages.html')
+
+@bp.route('users')
+@register_menu(bp, '.admin.users', 'Users')
+def users():
+    return render_template('admin/users.html')
