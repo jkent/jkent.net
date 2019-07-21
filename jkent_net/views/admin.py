6f78f14821b94194654d509a6be9dd3044cbad26
Jeff Kent <jeff@jkent.net>
2019-07-20 22:42:28 -0500

    admin stuff

--- jkent_net/views/admin.py
+++ jkent_net/views/admin.py
@@ -1,10 +1,9 @@
-from ..models import Page, User
+from ..models import Page, Role, User
 from ..utils import Pager
 from flask import Blueprint, render_template, request
 from flask_menu import current_menu, register_menu
 from flask_security import roles_accepted
 from flask_security.core import current_user
-
 
 __all__ = ['bp']
 
@@ -19,18 +18,51 @@ def settings():
 
 @bp.route('pages')
 @roles_accepted('admin', 'editor')
-@register_menu(bp, '.admin.pages', 'Pages', visible_when=lambda: current_user.any_role('admin', 'editor'))
-def pages():
+@register_menu(bp, '.admin.pages', 'Pages',
+    visible_when=lambda: current_user.any_role('admin', 'editor'),
+    active_when=lambda: request.endpoint.startswith('admin.pages_'))
+def pages_index():
     pager = Pager(20, Page.query.count())
     pager.page = int(request.args.get('page', pager.first))
     pages = Page.query.order_by(Page.name).offset(pager.offset).limit(pager.items_per_page).all()
-    return render_template('admin/pages.html', pager=pager, pages=pages)
+    return render_template('admin/pages_index.html', pager=pager, pages=pages)
+
+@bp.route('pages/<string:id>')
+@roles_accepted('admin', 'editor')
+@register_menu(bp, '.admin.pages.edit', 'Edit')
+def pages_edit(id):
+    return render_template('admin/pages_edit.html')
+
+@bp.route('roles')
+@roles_accepted('admin')
+@register_menu(bp, '.admin.roles', 'Roles',
+    visible_when=lambda: current_user.any_role('admin'),
+    active_when=lambda: request.endpoint.startswith('admin.roles_'))
+def roles_index():
+    pager = Pager(20, Role.query.count())
+    pager.page = int(request.args.get('page', pager.first))
+    roles = Role.query.order_by(Role.description).offset(pager.offset).limit(pager.items_per_page).all()
+    return render_template('admin/roles_index.html', pager=pager, roles=roles)
+
+@bp.route('roles/<int:id>')
+@roles_accepted('admin')
+@register_menu(bp, '.admin.roles.edit', 'Edit')
+def roles_edit(id):
+    return render_template('admin/roles_edit.html')
 
 @bp.route('users')
 @roles_accepted('admin')
-@register_menu(bp, '.admin.users', 'Users', visible_when=lambda: current_user.any_role('admin'))
-def users():
+@register_menu(bp, '.admin.users', 'Users',
+    visible_when=lambda: current_user.any_role('admin'),
+    active_when=lambda: request.endpoint.startswith('admin.users_'))
+def users_index():
     pager = Pager(20, User.query.count())
     pager.page = int(request.args.get('page', pager.first))
     users = User.query.order_by(User.name, User.email).offset(pager.offset).limit(pager.items_per_page).all()
-    return render_template('admin/users.html', pager=pager, users=users)
+    return render_template('admin/users_index.html', pager=pager, users=users)
+
+@bp.route('users/<int:id>')
+@roles_accepted('admin')
+@register_menu(bp, '.admin.users.edit', 'Edit')
+def users_edit(id):
+    return render_template('admin/users_edit.html')
